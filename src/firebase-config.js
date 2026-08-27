baae9bc67c3997b6c7cb443f3444240161d52ea6
Auhona Chakraborty <117756328+tomting-Auhona@users.noreply.github.com>
2026-08-27 14:24:43 +0700

    database

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,9 +1,21 @@
 export const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY",
-  authDomain: "PASTE_YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://PASTE_YOUR_DATABASE_URL.firebasedatabase.app",
-  projectId: "PASTE_YOUR_PROJECT_ID",
-  storageBucket: "PASTE_YOUR_PROJECT.firebasestorage.app",
-  messagingSenderId: "PASTE_YOUR_MESSAGING_SENDER_ID",
-  appId: "PASTE_YOUR_APP_ID"
+  apiKey: "AIzaSyC-DDQIIxoDEckr77UcVR-uYuPkCzYoIZk",
+
+  authDomain:
+    "ghost-board-df3e9.firebaseapp.com",
+
+  databaseURL:
+    "https://ghost-board-df3e9-default-rtdb.asia-southeast1.firebasedatabase.app/",
+
+  projectId:
+    "ghost-board-df3e9",
+
+  storageBucket:
+    "ghost-board-df3e9.firebasestorage.app",
+
+  messagingSenderId:
+    "375113459519",
+
+  appId:
+    "1:375113459519:web:b82da4222b094ccd00958a"
 };
